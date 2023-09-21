c6ad7e000b9700cfb2b450df40ee05e48efba4e1
Mohit Dubey <63240844+mohit-dubey66@users.noreply.github.com>
2023-09-21 08:12:37 +0000

    learnt some basics of JS

--- 01_Basics/datatypes.js
+++ 01_Basics/datatypes.js
@@ -1,1 +1,25 @@
-"use scripts"  // strictly use newer version of JavaScripts
+"use strict";  // treat all JS code as newer version
+
+// NOTE: always write readable code 
+// JS Specifications: https://tc39.es/ecma262
+
+
+// number => 2 to the power 53
+// bigint => to store big numbers, mainly used in stock market related things
+// string => ""
+// boolean => true/false
+// null => standalone value or representation of an empty value
+    // E.g., let temperature = null;
+// undefined => variable declare kiya hai but value nhi diya.
+    // E.g., let state;
+// symbol => unique representation
+
+// object
+// -------------------------------------------------------
+
+// How to check datatype of a variable
+
+console.log(typeof 23);         // number
+console.log(typeof "Mohit");    // string
+console.log(typeof null);       // object
+console.log(typeof undefined);  // undefined
